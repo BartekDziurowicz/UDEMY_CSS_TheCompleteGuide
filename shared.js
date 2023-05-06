8adccea9c535fd48ef2a1f3bf830e50795ad5f7a
BartekDziurowicz <bartosz.dziurowicz@gmail.com>
2023-05-06 08:44:57 +0200

    working with javascript and css

---
+++ shared.js
@@ -0,0 +1,44 @@
+var backdrop = document.querySelector('.backdrop');
+var modal = document.querySelector('.modal');
+var selectPlanButton = document.querySelectorAll('.plan button');
+var modalNoButton = document.querySelector('.modal__action--negative');
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+
+for (var i=0; i<selectPlanButton.length; i++) {
+    selectPlanButton[i].addEventListener('click', function() {
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        // modal.className = 'open';
+        modal.classList.add('open');
+        backdrop.classList.add('open');
+    });
+}
+
+backdrop.addEventListener('click', function() {
+    mobileNav.classList.remove('open');
+    // modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+    if (modal) {
+        modal.classList.remove('open');
+    }
+    backdrop.classList.remove('open');
+});
+
+if (modalNoButton) {
+    modalNoButton.addEventListener('click', function() {
+        // modal.style.display = 'none';
+        // backdrop.style.display = 'none';
+        if (modal) {
+            modal.classList.remove('open');
+        }
+        backdrop.classList.remove('open');
+    });
+}
+
+toggleButton.addEventListener('click', function() {
+    // mobileNav.style.display = 'block';
+    // backdrop.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+});
